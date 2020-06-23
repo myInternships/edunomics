6f87f9b9c1c971090ceb842be650453906b94c3d
pd-escher <dhoolia.pranav@gmail.com>
2020-06-23 22:46:15 +0530

    add frontend

---
+++ client/build/precache-manifest.d7f90a070447965286853e2938f448f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2049b58453a7bde3af797118a321b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ef845dfeb53b69af745",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b4f6f3910449da7924ac",
+    "url": "/static/js/2.7203c799.chunk.js"
+  },
+  {
+    "revision": "27200a5499d660fd8253e73b0c4cf016",
+    "url": "/static/js/2.7203c799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef845dfeb53b69af745",
+    "url": "/static/js/main.69d71f8c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
